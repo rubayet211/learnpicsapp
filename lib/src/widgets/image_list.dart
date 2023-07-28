04eabacbb3d7dc159599bc7e5a51f2913fa34204
rubayet211 <rubayet211@gmail.com>
2023-07-29 03:54:43 +0600

    seventh commit

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -5,10 +5,12 @@ class ImageList extends StatelessWidget {
   final List<ImageModel> images;
   ImageList(this.images);
 
-  Widget built(context) {
+  Widget build(context) {
     return ListView.builder(
       itemCount: images.length,
-      itemBuilder: (context, int index) {},
+      itemBuilder: (context, int index) {
+        return Image.network(images[index].url);
+      },
     );
   }
 }
