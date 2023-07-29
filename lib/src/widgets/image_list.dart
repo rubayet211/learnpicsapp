d7b781a717fa7f02e1cfdb94308d9adf19a3a838
rubayet211 <rubayet211@gmail.com>
2023-07-29 10:47:34 +0600

    10th Commit

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -21,7 +21,15 @@ class ImageList extends StatelessWidget {
       ),
       padding: EdgeInsets.all(20.0),
       margin: EdgeInsets.all(20.0),
-      child: Image.network(image.url),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 8.0),
+          ),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
